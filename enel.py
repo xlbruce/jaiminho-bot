1e9f5b9d5a2224df1a87fcc57c65124841aee9d0
Gilson de Paula <gilsonsvieira93@gmail.com>
2019-03-15 00:08:38 -0300

    Added Enel (Eletropaulo) module

---
+++ enel.py
@@ -0,0 +1,39 @@
+import json
+import locale
+
+from datetime import datetime
+from requests import Session
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+jinja = Environment(
+    loader=FileSystemLoader('templates'),
+)
+
+def format_invoice(invoice):
+    due_date = datetime.strptime(invoice['VENCIMENTO'], '%Y%m%d').strftime('%d/%m/%Y')
+    value = locale.currency(invoice['MONTANTE'])
+    return {
+        'due_date': due_date,
+        'value': value,
+        'barcode': invoice['O_COD_BARRAS']
+    }
+
+
+def get_invoices(user_credentials):
+    """`user_credentials` is a dict containing `cpf` and `instalacao` keys""" 
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36'
+    login_payload = jinja.get_template('payload.json').render(user_credentials)
+
+    session = Session()
+    session.headers.update({'user-agent': user_agent})
+    login = session.post('https://portalhome.eneldistribuicaosp.com.br/api/sap/getloginv2', json=json.loads(login_payload))
+    session.headers.update({'Authorization': login.headers['Authorization']})
+
+    portalinfo_payload = json.loads('{"I_CANAL":"ZINT","I_COD_SERV":"TC","I_SSO_GUID":""}')
+    portalinfo = session.post('https://portalhome.eneldistribuicaosp.com.br/api/sap/portalinfo', json=portalinfo_payload)
+    invoices = json.loads(portalinfo.text).get('ET_CONTAS')
+    open_invoices = filter(lambda invoice: 'Pendente' in invoice['SITUACAO'], invoices)
+
+    return map(format_invoice, open_invoices)
